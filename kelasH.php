ea7a4246289b98233a6544b9ec8a80b479f89d7f
ranggakpw <55146989+ranggakpw@users.noreply.github.com>
2019-09-12 08:08:57 +0700

    create

---
+++ kelasH.php
@@ -0,0 +1,16 @@
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box-3 bg-red size_box1 hover-zoom-effect">
+                        <div class="icon">
+                            <i class="material-icons">beenhere</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">Total Nota Dinas</div>
+                            <div class="number">{{ $on_progress['nota_dinas'] }}</div>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+
+<div>
+echo "Hai Kelas H";
+</div>
